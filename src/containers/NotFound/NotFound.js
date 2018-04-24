7f1f3e651cf8bfbb06e5d981f1f0348dfbed8ada
Caio Amaral Corrêa <caiobep@me.com>
2018-04-24 19:27:45 -0300

    Not Found container

---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'typeface-gugi'
+import './NotFound.css'
+
+const NotFound = () => (
+  <div className="not-found">
+    <h1> 404 </h1>
+    <span> This page does not exists</span>
+    <img src="https://media.giphy.com/media/jWexOOlYe241y/giphy.gif" alt="not-found" />
+  </div>
+)
+
+export default NotFound
